a66b5c42eac709b249d0b2eb7fff37aa43f9dd9e
SaeedSoleymani79 <110929202+SaeedSoleymani79@users.noreply.github.com>
2026-04-17 22:54:07 +0330

    Update and rename app.py to streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import librosa
 import numpy as np
 
+st.title("🎈 My new Streamlit app")
+
 # Standard Major/Minor profiles (Krumhansl-Kessler)
 MAJOR_PROFILE = [6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88]
 MINOR_PROFILE = [6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]
